8eedbd91679bc0f182cc8b0d80727bb807832555
prabhat <lxapp@linkcxo.com>
2023-05-17 18:04:02 +0530

    corporate module update

---
+++ LinkcxoWeb/src/corporate/Corporate.java
@@ -0,0 +1,31 @@
+package corporate;
+
+import org.openqa.selenium.By;
+
+import commonClassLinkcxo.AuthCommon;
+import commonClassLinkcxo.Superclass;
+
+public class Corporate extends Superclass{
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Login
+		AuthCommon auth = new AuthCommon();
+		auth.login();
+		Thread.sleep(6000);
+
+		//click on three dot tab
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/header/div/div/div[2]/div[3]/div[4]/div[2]/h2")).click();
+		Thread.sleep(3000);
+		//click on three dot tab
+		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/main/div/div[1]/div/div/div[2]/div/div/div/div/div/div/div[1]/div/img")).click();
+		Thread.sleep(3000);
+				
+		//click on three dot tab
+		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/main/div/div[1]/div/div/div[3]/div/div/div/div[1]/div/div[2]/div[5]/div/div[2]/div[2]/div/button")).click();
+		Thread.sleep(3000);
+		
+				
+	}
+	
+}
