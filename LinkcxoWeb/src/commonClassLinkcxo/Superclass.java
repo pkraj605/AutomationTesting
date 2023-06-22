e71bb3df3ebfdfd917aedd0ed458030e631eb861
Prabhat <Prabhat@LAPTOP-N8IM52PN>
2023-06-22 14:28:08 +0530

    jar file added

--- LinkcxoWeb/src/commonClassLinkcxo/Superclass.java
+++ LinkcxoWeb/src/commonClassLinkcxo/Superclass.java
@@ -7,13 +7,14 @@ public class Superclass
 {
 	public static WebDriver driver;
 	public Superclass(){
-		System.setProperty("webdriver.chrome.driver","C:/Users/prabh/Downloads/chromedriver_win32/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:/Users/Prabhat/Downloads/chromedriver_win32/chromedriver.exe");
+													
 		driver = new ChromeDriver();
 	}
 
 	public static  WebDriver getdriver(){
 		if (driver == null){
-			System.setProperty("webdriver.chrome.driver","C:/Users/prabh/Downloads/chromedriver_win32/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver","C:/Users/Prabhat/Downloads/chromedriver_win32/chromedriver.exe");
 			driver = new ChromeDriver();
 			return driver;
 		}else{
